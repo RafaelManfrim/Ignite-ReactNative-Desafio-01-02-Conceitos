1cee6122dc273e49b9f36a1752aad834455ac3ed
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-01-16 20:58:41 -0300

    TaskItem e alert de remoção de task

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import { FlatList, Image, TouchableOpacity, View, Text, StyleSheet, FlatListProps } from 'react-native';
-import Icon from 'react-native-vector-icons/Feather';
-
-import { ItemWrapper } from './ItemWrapper';
-
-import trashIcon from '../assets/icons/trash/trash.png'
+import { FlatList, FlatListProps } from 'react-native';
+import { TaskItem } from './TaskItem';
 
 export interface Task {
   id: number;
@@ -26,39 +22,7 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
       contentContainerStyle={{ paddingBottom: 24 }}
       showsVerticalScrollIndicator={false}
       renderItem={({ item, index }) => {
-        return (
-          <ItemWrapper index={index}>
-            <View>
-              <TouchableOpacity
-                testID={`button-${index}`}
-                activeOpacity={0.7}
-                style={styles.taskButton}
-                onPress={() => toggleTaskDone(item.id)}
-              >
-                <View 
-                  testID={`marker-${index}`}
-                  style={item.done ? styles.taskMarkerDone : styles.taskMarker}
-                >
-                  { item.done && (
-                    <Icon name="check" size={12} color="#FFF" />
-                  )}
-                </View>
-
-                <Text style={item.done ? styles.taskTextDone : styles.taskText}>
-                  {item.title}
-                </Text>
-              </TouchableOpacity>
-            </View>
-
-            <TouchableOpacity
-              testID={`trash-${index}`}
-              style={{ paddingHorizontal: 24 }}
-              onPress={() => removeTask(item.id)}
-            >
-              <Image source={trashIcon} />
-            </TouchableOpacity>
-          </ItemWrapper>
-        )
+        return <TaskItem item={item} index={index} toggleTaskDone={toggleTaskDone} removeTask={removeTask} />
       }}
       style={{
         marginTop: 32
@@ -66,43 +30,3 @@ export function TasksList({ tasks, toggleTaskDone, removeTask }: TasksListProps)
     />
   )
 }
-
-const styles = StyleSheet.create({
-  taskButton: {
-    flex: 1,
-    paddingHorizontal: 24,
-    paddingVertical: 15,
-    marginBottom: 4,
-    borderRadius: 4,
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  taskMarker: {
-    height: 16,
-    width: 16,
-    borderRadius: 4,
-    borderWidth: 1,
-    borderColor: '#B2B2B2',
-    marginRight: 15,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  taskText: {
-    color: '#666',
-    fontFamily: 'Inter-Medium'
-  },
-  taskMarkerDone: {
-    height: 16,
-    width: 16,
-    borderRadius: 4,
-    backgroundColor: '#1DB863',
-    marginRight: 15,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  taskTextDone: {
-    color: '#1DB863',
-    textDecorationLine: 'line-through',
-    fontFamily: 'Inter-Medium'
-  }
-})
